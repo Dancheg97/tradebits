e8a7fd7e77db602b052d7ab4b10dbd5d3258cc2d
Danila <dangdancheg@gmail.com>
2021-05-26 20:09:38 +0300

    .

--- ____tests/asset_tests2/main.go
+++ ____tests/asset_tests2/main.go
@@ -5,26 +5,21 @@ import (
 	"sync_tree/asset"
 )
 
-/*
-make trade
-make existed
-make with close
-make with open
-*/
-
-func sellMatches(sell asset.Sell, buy asset.Buy) bool {
-	return float64(sell.Offer/sell.Recieve) > float64(buy.Recieve/buy.Offer)
-}
-
-func fullForSeller(sell asset.Sell, buy asset.Buy) bool {
-	return buy.Offer >= sell.Recieve
-}
-
-func closeSeller(sell asset.Sell, buy asset.Buy) (asset.Sell, asset.Buy) {
+func operateSellToBuy(sell asset.Sell, buy asset.Buy) (asset.Sell, asset.Buy) {
+	if float64(sell.Offer/sell.Recieve) < float64(buy.Recieve/buy.Offer) {
+		return sell, buy
+	}
+	if buy.Offer < sell.Recieve {
+		sell.Offer = sell.Offer - buy.Recieve
+		sell.Recieve = sell.Recieve - buy.Offer
+		buy.Offer = 0
+		buy.Recieve = 0
+		return sell, buy
+	}
 	newSellerOffer := uint64(buy.Recieve / buy.Offer *
 		(buy.Offer - sell.Recieve))
 	newBuyOffer := buy.Offer - sell.Recieve
-	newBuyRecieve := sell.Offer - newSellerOffer
+	newBuyRecieve := sell.Offer + buy.Recieve
 	sell.Offer = newSellerOffer
 	sell.Recieve = 0
 	buy.Offer = newBuyOffer
@@ -32,31 +27,16 @@ func closeSeller(sell asset.Sell, buy asset.Buy) (asset.Sell, asset.Buy) {
 	return sell, buy
 }
 
-func closeBuyer(sell asset.Sell, buy asset.Buy) (asset.Sell, asset.Buy) {
-	sell.Offer = sell.Offer - buy.Recieve
-	sell.Recieve = sell.Recieve - buy.Offer
-	buy.Offer = 0
-	buy.Recieve = 0
-	return sell, buy
-}
-
 func main() {
 	sell := asset.Sell{
-		Offer:   900,
+		Offer:   500,
 		Recieve: 100,
 	}
 
 	buy := asset.Buy{
 		Offer:   50,
-		Recieve: 400,
+		Recieve: 5000,
 	}
 
-	if sellMatches(sell, buy) {
-		if fullForSeller(sell, buy) {
-			fmt.Println(closeSeller(sell, buy))
-		} else {
-			fmt.Println(closeBuyer(sell, buy))
-		}
-	}
-
+	fmt.Println(operateSellToBuy(sell, buy))
 }
