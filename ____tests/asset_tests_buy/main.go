69bc82a093df1cb48fd163a7c8df29cb78d2b2d4
Danila Fomin <dangdancheg@gmail.com>
2021-05-26 23:12:24 +0300

    .

--- ____tests/asset_tests_buy/main.go
+++ ____tests/asset_tests_buy/main.go
@@ -9,7 +9,11 @@ func checkMatch(sell asset.Sell, buy asset.Buy) bool {
 	return float64(buy.Recieve/buy.Offer) < float64(sell.Offer/sell.Recieve)
 }
 
-func test_non_match() {
+func restSeller(sell asset.Sell, buy asset.Buy) bool {
+	return sell.Recieve > buy.Offer
+}
+
+func testMatch() {
 	sell := asset.Sell{
 		Offer:   500,
 		Recieve: 100,
@@ -25,6 +29,44 @@ func test_non_match() {
 	fmt.Println("\033[31m[TEST] (ASSET_BUY) {MATCH} - failed\033[0m")
 }
 
+func testNonMatch() {
+	sell := asset.Sell{
+		Offer:   500,
+		Recieve: 100,
+	}
+	buy := asset.Buy{
+		Offer:   100,
+		Recieve: 600,
+	}
+	if checkMatch(sell, buy) {
+		fmt.Println("\033[31m[TEST] (ASSET_BUY) {NON_MATCH} - failed\033[0m")
+		return
+	}
+	fmt.Println("\033[32m[TEST] (ASSET_BUY) {NON_MATCH} - passed\033[0m")
+}
+
+func testCloseSeller() {
+	sell := asset.Sell{
+		Offer:   500,
+		Recieve: 100,
+	}
+	buy := asset.Buy{
+		Offer:   80,
+		Recieve: 600,
+	}
+	if checkMatch(sell, buy) {
+		if restSeller(sell, buy) {
+			fmt.Println(
+				"\033[32m[TEST] (ASSET_BUY) {REST_SELLER} - passed\033[0m",
+			)
+			return
+		}
+	}
+	fmt.Println("\033[31m[TEST] (ASSET_BUY) {REST_SELLER} - failed\033[0m")
+}
+
 func main() {
-	test_non_match()
+	testMatch()
+	testNonMatch()
+	testCloseSeller()
 }
