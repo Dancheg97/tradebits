378a7bc6a056b45c426e6b88ce378b898612b212
Danila <dangdancheg@gmail.com>
2021-05-28 21:56:55 +0300

    .

---
+++ market/buy.go
@@ -0,0 +1,7 @@
+package market
+
+type Buy struct {
+	Adress        []byte
+	OfferMain     uint64
+	RecieveMarket uint64
+}
--- market/sell.go
+++ market/sell.go
@@ -1,25 +1,23 @@
 package market
 
-import "math"
-
-type Buy struct {
-	Adress        []byte
-	OfferMain     uint64
-	RecieveMarket uint64
-}
-
 type Sell struct {
 	Adress      []byte
 	OfferMarket uint64
 	RecieveMain uint64
 }
 
-func (sell Sell) checkMatch(buy Buy) bool {
+func (sell *Sell) checkMatch(buy Buy) bool {
 	sellRatio := float64(buy.RecieveMarket / buy.OfferMain)
 	buyRatio := float64(sell.OfferMarket / sell.RecieveMain)
 	return sellRatio < buyRatio
 }
 
-func (sell Sell) selfClose(buy Buy) bool {
+func (sell *Sell) selfClose(buy Buy) bool {
 	return sell.RecieveMain < buy.OfferMain
 }
+
+func (sell Sell) closeSell(buy Buy) (Buy, output, output) {
+
+}
+
+func (sell Sell) closeBuy(buy Buy) 
