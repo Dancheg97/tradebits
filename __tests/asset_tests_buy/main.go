4cc6607a859d2c54a177556ded7eedacebaf0185
Danila Fomin <dangdancheg@gmail.com>
2021-05-28 11:54:54 +0300

    .

--- __tests/asset_tests_buy/main.go
+++ __tests/asset_tests_buy/main.go
@@ -14,7 +14,7 @@ func testMatch() {
 		OfferMain:    100,
 		RecieveAsset: 400,
 	}
-	if asset.B2SCheckMatch(sell, buy) {
+	if asset.S2BCheckMatch(sell, buy) {
 		__tests.Passed("asset", "Buy/Match", "checks if trade matches")
 		return
 	}
@@ -46,8 +46,8 @@ func testIfSellIsClosing() {
 		OfferMain:    80,
 		RecieveAsset: 200,
 	}
-	if asset.B2SCheckMatch(sell, buy) {
-		if asset.B2SIfCloseSeller(sell, buy) {
+	if asset.S2BCheckMatch(sell, buy) {
+		if asset.S2BIfCloseSeller(sell, buy) {
 			__tests.Failed("asset", "Buy/Sell-", "checks if trade closes sell")
 			return
 		}
@@ -64,8 +64,8 @@ func testIfBuyIsClosing() {
 		OfferMain:    800,
 		RecieveAsset: 2000,
 	}
-	if asset.B2SCheckMatch(sell, buy) {
-		if asset.B2SIfCloseSeller(sell, buy) {
+	if asset.S2BCheckMatch(sell, buy) {
+		if asset.S2BIfCloseSeller(sell, buy) {
 			__tests.Passed("asset", "Buy/Buy-", "checks if trade closes buy")
 			return
 		}
@@ -82,26 +82,26 @@ func testCloseSell() {
 		OfferMain:    120,
 		RecieveAsset: 200,
 	}
-	if asset.B2SCheckMatch(sell, buy) {
-		if asset.B2SIfCloseSeller(sell, buy) {
-			newBuy, sellOut, buyOut := asset.B2SCloseSeller(sell, buy)
+	if asset.S2BCheckMatch(sell, buy) {
+		if asset.S2BIfCloseSeller()(sell, buy) {
+			newBuy, sellOut, buyOut := asset.S2BCloseSeller()(sell, buy)
 			ch1 := sellOut.MainOut == 100
 			ch2 := sellOut.AssetOut == 333
 			ch3 := buyOut.AssetOut == 167
 			ch4 := newBuy.OfferMain == 20
 			ch5 := newBuy.RecieveAsset == 33
 			if ch1 && ch2 && ch3 && ch4 && ch5 {
-				__tests.Passed("asset", "CloseSeller", "pretty good numbers")
+				__tests.Passed("asset", "Buy/CLSell", "pretty good numbers")
 				return
 			}
-			__tests.Failed("asset", "CloseSeller", "bad numbers")
+			__tests.Failed("asset", "Buy/CLSell", "bad numbers")
 			return
 		} else {
-			__tests.Failed("asset", "CloseSeller", "not closing")
+			__tests.Failed("asset", "Buy/CLSell", "not closing")
 			return
 		}
 	}
-	__tests.Failed("asset", "CloseSeller", "not even matching")
+	__tests.Failed("asset", "Buy/CLSell", "not even matching")
 }
 
 func testCloseBuyer() {
@@ -113,25 +113,25 @@ func testCloseBuyer() {
 		OfferMain:    80,
 		RecieveAsset: 200,
 	}
-	if asset.B2SCheckMatch(sell, buy) {
-		if asset.B2SIfCloseSeller(sell, buy) {
-			__tests.Failed("asset", "CloseBuyer", "not closing")
+	if asset.S2BCheckMatch(sell, buy) {
+		if asset.S2BIfCloseSeller()(sell, buy) {
+			__tests.Failed("asset", "Buy/CLBuy", "not closing")
 			return
 		} else {
-			newSell, sellOut, buyOut := asset.B2SCloseBuyer(sell, buy)
+			newSell, sellOut, buyOut := asset.S2BCloseBuyer(sell, buy)
 			ch1 := sellOut.MainOut == 80
 			ch2 := buyOut.AssetOut == 200
 			ch3 := newSell.OfferAsset == 300
 			ch4 := newSell.RecieveMain == 20
 			if ch1 && ch2 && ch3 && ch4 {
-				__tests.Passed("asset", "CloseBuyer", "pretty good numbers")
+				__tests.Passed("asset", "Buy/CLBuy", "pretty good numbers")
 				return
 			}
-			__tests.Failed("asset", "CloseBuyer", "bad numbers")
+			__tests.Failed("asset", "Buy/CLBuy", "bad numbers")
 			return
 		}
 	}
-	__tests.Failed("asset", "CloseBuyer", "not even matching")
+	__tests.Failed("asset", "Buy/CLBuy", "not even matching")
 }
 
 func main() {
