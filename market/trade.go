6cfd7c9a7dfb190edb07e55dbd5154d3e1b7f752
Danila Fomin <dangdancheg@gmail.com>
2021-05-30 13:58:39 +0300

    .

--- market/trade.go
+++ market/trade.go
@@ -1,5 +1,7 @@
 package market
 
+import "math"
+
 type Trade struct {
 	Adress  []byte
 	IsSell  bool
@@ -7,6 +9,46 @@ type Trade struct {
 	Recieve uint64
 }
 
-func (new Trade) operate(old Trade) {
-
+func (new Trade) operate(old Trade) (bool, []Trade, []output) {
+	if new.Recieve < old.Offer {
+		ratio := float64(old.Recieve) / float64(old.Offer)
+		potentialNewOffer := uint64(math.Ceil(float64(new.Recieve) * ratio))
+		if potentialNewOffer > new.Offer {
+			return false, []Trade{new, old}, nil
+		}
+		if new.IsSell {
+			newOutput := output{
+				Adress:    new.Adress,
+				MainOut:   new.Offer - potentialNewOffer,
+				MarketOut: new.Recieve,
+			}
+			oldOutput := output{
+				Adress:  old.Adress,
+				MainOut: potentialNewOffer,
+			}
+			old.Offer = old.Offer - new.Recieve
+			old.Recieve = old.Recieve - potentialNewOffer
+			return true, []Trade{old}, []output{newOutput, oldOutput}
+		}
+		newOutput := output{
+			Adress:    new.Adress,
+			MarketOut: new.Offer - potentialNewOffer,
+			MainOut:   new.Recieve,
+		}
+		oldOutput := output{
+			Adress:    old.Adress,
+			MarketOut: potentialNewOffer,
+		}
+		old.Offer = old.Offer - new.Recieve
+		old.Recieve = old.Recieve - potentialNewOffer
+		return true, []Trade{old}, []output{newOutput, oldOutput}
+	}
+	newRatio := float64(new.Offer / new.Recieve)
+	oldRatio := float64(old.Offer / new.Recieve)
+	if newRatio < oldRatio {
+		return false, []Trade{old, new}, []output{}
+	}
+	if new.IsSell {
+		
+	}
 }
