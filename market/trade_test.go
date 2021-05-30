6c2f0027da17689df676e9df64edfcfa74103372
Danila Fomin <dangdancheg@gmail.com>
2021-05-30 16:15:10 +0300

    .

--- market/trade_test.go
+++ market/trade_test.go
@@ -1,6 +1,9 @@
 package market
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestOperate(t *testing.T) {
 	for i := 0; i < 10000; i++ {
@@ -12,6 +15,16 @@ func TestOperate(t *testing.T) {
 			Adress: []byte("c"),
 			IsSell: false,
 		}
+		randNumbers := []uint64{}
+		for i := 0; i < 4; i++ {
+			num := uint64(rand.Uint32())<<32 + uint64(rand.Uint32())
+			randNumbers = append(randNumbers, num)
+		}
+		sell.Offer = randNumbers[0]
+		sell.Recieve = randNumbers[1]
+		buy.Offer = randNumbers[2]
+		buy.Recieve = randNumbers[3]
+		sell.operate(buy)
 		
 	}
 }
