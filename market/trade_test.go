7d255ca76f4bc86feeab7c3b04d24b271d4501e9
Danila Fomin <dangdancheg@gmail.com>
2021-05-30 16:10:25 +0300

    .

--- market/trade_test.go
+++ market/trade_test.go
@@ -3,20 +3,15 @@ package market
 import "testing"
 
 func TestOperate(t *testing.T) {
-	buy := Trade{
-		Adress:  []byte("b"),
-		IsSell:  true,
-		Offer:   400,
-		Recieve: 300,
+	for i := 0; i < 10000; i++ {
+		sell := Trade{
+			Adress: []byte("b"),
+			IsSell: true,
+		}
+		buy := Trade{
+			Adress: []byte("c"),
+			IsSell: false,
+		}
+		
 	}
-	sell := Trade{
-		Adress:  []byte("ss"),
-		IsSell:  false,
-		Offer:   400,
-		Recieve: 600,
-	}
-	operated, trades, outputs := buy.operate(sell)
-	t.Error(operated)
-	t.Error(trades)
-	t.Error(outputs)
 }
