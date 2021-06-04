918269018a023da2232ccddd1e492fb7da961bdd
Danila Fomin <dangdancheg@gmail.com>
2021-06-05 00:51:35 +0300

    .

--- api/api_grpc.pb.go
+++ api/api_grpc.pb.go
@@ -18,7 +18,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type SyncTreeClient interface {
-	AddUser(ctx context.Context, in *UserCreateRequest, opts ...grpc.CallOption) (*Response, error)
+	UserCreate(ctx context.Context, in *UserCreateRequest, opts ...grpc.CallOption) (*Response, error)
+	UserSend(ctx context.Context, in *UserSendRequest, opts ...grpc.CallOption) (*Response, error)
 }
 
 type syncTreeClient struct {
@@ -29,9 +30,18 @@ func NewSyncTreeClient(cc grpc.ClientConnInterface) SyncTreeClient {
 	return &syncTreeClient{cc}
 }
 
-func (c *syncTreeClient) AddUser(ctx context.Context, in *UserCreateRequest, opts ...grpc.CallOption) (*Response, error) {
+func (c *syncTreeClient) UserCreate(ctx context.Context, in *UserCreateRequest, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/api.SyncTree/AddUser", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/api.SyncTree/UserCreate", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *syncTreeClient) UserSend(ctx context.Context, in *UserSendRequest, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/api.SyncTree/UserSend", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +52,8 @@ func (c *syncTreeClient) AddUser(ctx context.Context, in *UserCreateRequest, opt
 // All implementations must embed UnimplementedSyncTreeServer
 // for forward compatibility
 type SyncTreeServer interface {
-	AddUser(context.Context, *UserCreateRequest) (*Response, error)
+	UserCreate(context.Context, *UserCreateRequest) (*Response, error)
+	UserSend(context.Context, *UserSendRequest) (*Response, error)
 	mustEmbedUnimplementedSyncTreeServer()
 }
 
@@ -50,8 +61,11 @@ type SyncTreeServer interface {
 type UnimplementedSyncTreeServer struct {
 }
 
-func (UnimplementedSyncTreeServer) AddUser(context.Context, *UserCreateRequest) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddUser not implemented")
+func (UnimplementedSyncTreeServer) UserCreate(context.Context, *UserCreateRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserCreate not implemented")
+}
+func (UnimplementedSyncTreeServer) UserSend(context.Context, *UserSendRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserSend not implemented")
 }
 func (UnimplementedSyncTreeServer) mustEmbedUnimplementedSyncTreeServer() {}
 
@@ -66,20 +80,38 @@ func RegisterSyncTreeServer(s grpc.ServiceRegistrar, srv SyncTreeServer) {
 	s.RegisterService(&SyncTree_ServiceDesc, srv)
 }
 
-func _SyncTree_AddUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _SyncTree_UserCreate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UserCreateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(SyncTreeServer).AddUser(ctx, in)
+		return srv.(SyncTreeServer).UserCreate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/api.SyncTree/AddUser",
+		FullMethod: "/api.SyncTree/UserCreate",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(SyncTreeServer).AddUser(ctx, req.(*UserCreateRequest))
+		return srv.(SyncTreeServer).UserCreate(ctx, req.(*UserCreateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SyncTree_UserSend_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UserSendRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SyncTreeServer).UserSend(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/api.SyncTree/UserSend",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SyncTreeServer).UserSend(ctx, req.(*UserSendRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -92,8 +124,12 @@ var SyncTree_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*SyncTreeServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "AddUser",
-			Handler:    _SyncTree_AddUser_Handler,
+			MethodName: "UserCreate",
+			Handler:    _SyncTree_UserCreate_Handler,
+		},
+		{
+			MethodName: "UserSend",
+			Handler:    _SyncTree_UserSend_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
