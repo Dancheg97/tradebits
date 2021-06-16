ca930fc2cff221ad9beef2dab6102814d8a35922
Danila Fomin <dangdancheg@gmail.com>
2021-06-16 17:27:31 +0300

    .

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -2,6 +2,8 @@ package calc
 
 import (
 	"crypto/x509"
+	"fmt"
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
@@ -9,6 +11,7 @@ import (
 
 func TestHash(t *testing.T) {
 	hash := Hash([]byte{0})
+	fmt.Println(hash)
 	expected := []byte{82, 183, 9, 232, 198, 142, 69, 166, 187, 232, 192, 96, 68, 75, 73, 151, 112, 183, 61, 164, 253, 193, 184, 5, 181, 233, 156, 208, 175, 166, 26, 164, 209, 27, 80, 16, 145, 0, 78, 104, 11, 151, 150, 131, 100, 121, 160, 43, 34, 196, 98, 251, 92, 80, 223, 142, 20, 163, 21, 232, 171, 85, 72, 39}
 	if reflect.DeepEqual(hash, expected) {
 		return
@@ -17,6 +20,7 @@ func TestHash(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
+	keyBytes, _ := ioutil.ReadFile("calc/priv.pem")
 	mes := []byte{1, 2, 3}
 	sign, _ := Sign([][]byte{mes, mes}, keyBytes)
 	if len(sign) == 512 {
@@ -26,6 +30,7 @@ func TestSign(t *testing.T) {
 }
 
 func TestVerify(t *testing.T) {
+	keyBytes, _ := ioutil.ReadFile("calc/priv.pem")
 	mes := []byte{1, 2, 3}
 	sign, _ := Sign([][]byte{mes, mes}, keyBytes)
 	priv, _ := x509.ParsePKCS1PrivateKey(keyBytes)
