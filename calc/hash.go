949745db355c23cd5832b0c989d219043d5dc973
Danila Fomin <dangdancheg@gmail.com>
2021-06-16 17:24:06 +0300

    .

--- calc/hash.go
+++ calc/hash.go
@@ -1,13 +1,12 @@
 package calc
 
 import (
-	"golang.org/x/crypto/blake2b"
+	"crypto/sha512"
 )
 
 // take hash from that byte array
 func Hash(bytes []byte) []byte {
-	hasher, _ := blake2b.New512([]byte("1u89hdsaj098as12"))
+	hasher := sha512.New()
 	hasher.Write(bytes)
-	hash := hasher.Sum(nil)
-	return hash
+	return hasher.Sum(nil)
 }
