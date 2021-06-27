85f95ec0945c72ffdc62caa2b475de43fe1e3496
Danila Fomin <dangdancheg@gmail.com>
2021-06-27 19:46:25 +0300

    added request to update market imformation

---
+++ market_create.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "sync_tree/api"
+	"sync_tree/calc"
+	"sync_tree/market"
+)
+
+func (s *server) MarketCreate(
+	ctx context.Context,
+	in *pb.MarketCreateRequest,
+) (*pb.Response, error) {
+	fmt.Println("got request to craete new market, name", in.Name)
+	concatedMessage := [][]byte{
+		in.PublicKey,
+		in.MesssageKey,
+		[]byte(in.Name),
+		[]byte(in.Img),
+		[]byte(in.Descr),
+	}
+	checkErr := calc.Verify(concatedMessage, in.PublicKey, in.Sign)
+	if checkErr == nil {
+		adress := calc.Hash(in.PublicKey)
+		craeteErr := market.Create(
+			adress,
+			in.Name,
+			in.MesssageKey,
+			in.Descr,
+			in.Img,
+		)
+		if craeteErr == nil {
+			return &pb.Response{Passed: true}, nil
+		}
+	}
+	return &pb.Response{Passed: false}, nil
+}
