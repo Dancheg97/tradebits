83a71a2420d1409207a6fcdb760ca269c8fdfa70
Danila Fomin <dangdancheg@gmail.com>
2021-07-03 17:25:38 +0300

    finished grpc api method for user to cancel some trade

--- user_cancel.go
+++ user_cancel.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	pb "sync_tree/api"
 	"sync_tree/calc"
+	"sync_tree/market"
 )
 
 func (s *server) UserCancelTrade(
@@ -12,14 +13,18 @@ func (s *server) UserCancelTrade(
 	in *pb.UserCancelTradeRequest,
 ) (*pb.Response, error) {
 	fmt.Println("cancel:", in.MarketAdress)
-	userAdress := calc.Hash(in.PublicKey)
 	concMes := [][]byte{
 		in.PublicKey,
 		in.MarketAdress,
 	}
 	verifyErr := calc.Verify(concMes, in.PublicKey, in.Sign)
 	if verifyErr == nil {
-		
+		userAdress := calc.Hash(in.PublicKey)
+		m := market.Get(in.MarketAdress)
+		if m != nil {
+			canceled := m.CancelTrades(userAdress)
+			return &pb.Response{Passed: canceled}, nil
+		}
 	}
 	return &pb.Response{Passed: false}, nil
 }
