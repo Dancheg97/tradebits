fd8661218752d115439ca847070f91f2ac33aeb7
Danila Fomin <dangdancheg@gmail.com>
2021-07-04 15:10:49 +0300

    updateing tests in database package

--- data/database.go
+++ data/database.go
@@ -18,10 +18,7 @@ func Get(key []byte) []byte {
 // put key by some value to database (if value exists use Change()
 // func instead)
 func Put(key []byte, value []byte) {
-	valueExists, unexpected := base.Has(key, nil)
-	if unexpected != nil {
-		return
-	}
+	valueExists, _ := base.Has(key, nil)
 	if valueExists {
 		return
 	}
--- data/database_test.go
+++ data/database_test.go
@@ -5,28 +5,17 @@ import (
 	"testing"
 )
 
-func TestDataBase(t *testing.T) {
+func TestPutValue(t *testing.T) {
 	testPutKey := []byte{1, 2, 3}
 	testPutValue := []byte{1, 2, 3, 4, 5}
-	testChangeValue := []byte{1, 2, 3, 4, 5, 6}
-	Put(testPutKey, testPutValue)
-
-	checked := Check(testPutKey)
-	if checked {
-	} else {
-		t.Error("failed to check item existens in database")
-	}
-
-	testGetValue := Get(testPutKey)
-	if reflect.DeepEqual(testGetValue, testPutValue) {
-	} else {
-		t.Error("failed to get item from database")
-	}
-
-	Change(testPutKey, testChangeValue)
-	changed := Get(testPutKey)
-	if reflect.DeepEqual(changed, testChangeValue) {
-	} else {
-		t.Error("failed to change item contents in database")
-	}
 }
+
+func TestPutExisting(t *testing.T) {
+
+}
+
+func TestGetValue(t *testing.T) {
+
+}
+
+func 
