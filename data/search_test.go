205445c524a843da70ecdd5d3a374e7801fd2420
Danila Fomin <dangdancheg@gmail.com>
2021-07-04 17:39:25 +0300

    finished remaking tests in data package, now its even better rewritten and outtested

--- data/search_test.go
+++ data/search_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestSearchAdd(t *testing.T) {
-	name := "name"
+	name := "name22"
 	adr := []byte{1, 2, 3}
 	SearchAdd(name, adr)
 	searcher.Delete(name)
 }
 
 func TestSearchSearch(t *testing.T) {
-	name := "name1"
+	name := "name122"
 	adr := []byte{1, 2, 3}
 	SearchAdd(name, adr)
 	rez := Search(name)
@@ -25,13 +25,17 @@ func TestSearchSearch(t *testing.T) {
 
 func TestSearchChange(t *testing.T) {
 	adr := []byte{1, 1, 1}
-	name1 := "Xname"
-	name2 := "Fname"
+	name1 := "Xname22"
+	name2 := "Fname22"
 	SearchAdd(name1, adr)
 	SearchChange(name2, adr)
 	rez := Search(name2)
-	if reflect.DeepEqual(rez[0], name2) {
-		return
+	if !reflect.DeepEqual(rez[0], adr) {
+		t.Error("found name should be equal to second")
 	}
-	t.Error("found name should be equal to second")
+	searcher.Delete("name2")
+}
+
+func TestSearchAddMultipleAdressesOnSameName(t *testing.T) {
+
 }
