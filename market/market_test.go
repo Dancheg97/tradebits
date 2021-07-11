ec3e6b130af6c1d4687aa68e8745dd9cbd319bf0
Danila Fomin <dangdancheg@gmail.com>
2021-07-11 15:45:14 +0300

    made a function to test ability to get market if it is busy

--- market/market_test.go
+++ market/market_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"sync_tree/data"
 	"testing"
+	"time"
 )
 
 func TestCreateNewMarket(t *testing.T) {
@@ -63,7 +64,22 @@ func TestAssetLook(t *testing.T) {
 }
 
 func TestMarketGetAfterBusy(t *testing.T) {
-	
+	var adress = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 129}
+	var mesKey = []byte{1, 2, 3, 4, 5}
+	var img = "asset image link . example"
+	var name = "newAsset"
+	var descr = "descrx"
+	Create(adress, name, mesKey, descr, img)
+	mkt1 := Get(adress)
+	mkt2 := Get(adress)
+	time.Sleep(time.Second)
+	mkt1.Save()
+	time.Sleep(time.Second)
+	if !reflect.DeepEqual(mkt2.adress, adress) {
+		t.Error("adresses should be equal")
+	}
+	mkt2.Save()
+	data.TestRM(adress)
 }
 
 func TestMarketPutMessage(t *testing.T) {
