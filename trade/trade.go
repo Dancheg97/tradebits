108f94b9efd4a5d1dfd858f4c657579470802cb2
Danila Fomin <dangdancheg@gmail.com>
2021-07-12 15:29:35 +0300

    added function to craete buy and sell functions

--- trade/trade.go
+++ trade/trade.go
@@ -1,4 +1,4 @@
-package market
+package trade
 
 type TradePool struct {
 	Buys    []Buy
--- trade/trade_test.go
+++ trade/trade_test.go
@@ -4,4 +4,14 @@ import (
 	"testing"
 )
 
-func TestBuyCreate
+func TestBuyCreation(t *testing.T) {
+	buy1 := NewBuy([]byte{0}, 1000, 1000)
+	if buy1.Offer != 1000 {
+		t.Error("buy offer should be equal to 1000")
+	}
+	buy2 := NewBuy([]byte{0}, 0, 1000)
+	if buy2 != nil {
+		t.Error("buy 2 should not be created due to 0 offer")
+	}
+}
+
