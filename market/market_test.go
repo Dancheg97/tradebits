791e6d8e6190341db38b3c1d89d5d8e09d03cebc
Danila Fomin <dangdancheg@gmail.com>
2021-09-20 16:50:02 +0300

    .

--- market/market_test.go
+++ market/market_test.go
@@ -208,25 +208,27 @@ func TestGetMarketThatDontExist(t *testing.T) {
 	}
 }
 
-// func TestMarketLook(t *testing.T) {
-// 	var adress = calc.Rand()
-// 	Create(
-// 		adress,
-// 		dummyName,
-// 		dummyMessageKey,
-// 		dummyDescription,
-// 		dummyImageLink,
-// 		dummyInputFee,
-// 		dummyOutputFee,
-// 		dummyWorkTime,
-// 		dummyDelimiter,
-// 	)
-// 	mkt := Look(adress)
-// 	if !reflect.DeepEqual(mkt.MesKey, mesKey) {
-// 		t.Error("keys are not the same, look asset error")
-// 	}
-// 	data.TestRM(adress)
-// }
+func TestMarketLook(t *testing.T) {
+	var adress = calc.Rand()
+	Create(
+		adress,
+		dummyName,
+		dummyMessageKey,
+		dummyDescription,
+		dummyImageLink,
+		dummyInputFee,
+		dummyOutputFee,
+		dummyWorkTime,
+		dummyDelimiter,
+	)
+	mkt := Look(adress)
+	if !reflect.DeepEqual(mkt.MesKey, dummyMessageKey) {
+		t.Error("keys are not the same, look asset error")
+	}
+	data.TestRM(adress)
+}
+
+// Here are market trading test logic gonna start, patience
 
 // func TestAttachUnboundedTrades(t *testing.T) {
 // 	var adress = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 229}
