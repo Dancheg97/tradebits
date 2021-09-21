702aee451366e9f12041aa1b793457d230041318
Danila Fomin <dangdancheg@gmail.com>
2021-09-22 02:34:33 +0300

    added methods to prepare server

--- test/prepare_server.go
+++ test/prepare_server.go
@@ -9,7 +9,6 @@ import (
 	"sync_tree/user"
 )
 
-
 func stringToKeyBytes(key string) []byte {
 	r := strings.NewReader(key)
 	pemBytes, _ := ioutil.ReadAll(r)
@@ -23,42 +22,46 @@ func createNewUsers() {
 	alcoMesKey := stringToKeyBytes(alcoSplitted[3])
 	user.Create(alcoAdress, alcoMesKey, "Alcohol")
 	alco := user.Get(alcoAdress)
-	defer alco.Save()
-	if alco.Balance == 0 {
-		alco.Balance = 50000
-	}
-	marketAdress := string([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 63})
-	if alco.Balances[marketAdress] == 0 {
-		alco.Balances[marketAdress] = 10000
-	}
-	// fmt.Println("alco wallet created with", alco.Balance, "balance")
+	alco.Balance = 50000
+	alco.Balances[string(alcoAdress)] = 10000
+	alco.Save()
+
 	nicoSplitted := strings.Split(nicoKeyString, "|")
 	nicoAdress := calc.Hash(stringToKeyBytes(nicoSplitted[1]))
 	nicoMesKey := stringToKeyBytes(nicoSplitted[3])
 	user.Create(nicoAdress, nicoMesKey, "Nicotin")
 	nico := user.Get(nicoAdress)
-	defer nico.Save()
-	if nico.Balance == 0 {
-		nico.Balance = 50000
-	}
-	// fmt.Println("nico wallet created with", nico.Balance, "balance")
+	nico.Balance = 80000
+	nico.Balances[string(nicoAdress)] = 4000
+	nico.Save()
 }
 
 func createStartMarket() {
+	alcoSplitted := strings.Split(alcoKeyString, "|")
+	alcoAdress := calc.Hash(stringToKeyBytes(alcoSplitted[1]))
 	market.Create(
-		[]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 63},
+		alcoAdress,
 		"Bitcoin Ftem",
-		[]byte{0, 1, 2, 3, 4, 2, 8},
+		dummyMessageKey,
 		"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
 		"https://image.flaticon.com/icons/png/512/1490/1490849.png",
-		1, 1, "+3GMT 7:00-9:00",
+		dummyInputFee,
+		dummyOutputFee,
+		dummyWorkTime,
+		dummyDelimiter,
 	)
+
+	nicoSplitted := strings.Split(nicoKeyString, "|")
+	nicoAdress := calc.Hash(stringToKeyBytes(nicoSplitted[1]))
 	market.Create(
-		[]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64},
-		"Sber ruble Ftem",
-		[]byte{0, 1, 2, 3, 4, 2, 8},
+		nicoAdress,
+		"Sber ruble ftem",
+		dummyMessageKey,
 		"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-		"https://image.flaticon.com/icons/png/512/1490/1490839.png",
-		1, 1, "+3GMT 7:00-9:00",
+		"https://image.flaticon.com/icons/png/512/1490/1490849.png",
+		dummyInputFee,
+		dummyOutputFee,
+		dummyWorkTime,
+		dummyDelimiter,
 	)
 }
