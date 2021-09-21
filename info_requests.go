a611d6ce1fc985f1da850bc0dbcab8ac787d5845
Danila Fomin <dangdancheg@gmail.com>
2021-09-21 15:30:37 +0300

    .

--- info_requests.go
+++ info_requests.go
@@ -16,60 +16,63 @@ func (s *server) InfoHasTrades(
 	in *pb.InfoHasTradesRequest,
 ) (*pb.Response, error) {
 	user := user.Look(in.UserAdress)
-	if user != nil {
-		market := market.Look(in.MarketAdress)
-		if market != nil {
-			hasTrades := market.HasTrades(in.UserAdress)
-			fmt.Sprintln("[InfoHasTrades] - has trades")
-			return &pb.Response{Passed: hasTrades}, nil
-		}
+	if user == nil {
+		fmt.Sprintln("[InfoHasTrades] - user not found")
+		return &pb.Response{Passed: false}, errors.New("user not found")
+	}
+	market := market.Look(in.MarketAdress)
+	if market == nil {
 		fmt.Sprintln("[InfoHasTrades] - market not found")
 		return &pb.Response{Passed: false}, errors.New("market not found")
 	}
-	fmt.Sprintln("[InfoHasTrades] - user not found")
-	return &pb.Response{Passed: false}, errors.New("user not found")
+	hasTrades := market.HasTrades(in.UserAdress)
+	fmt.Sprintln("[InfoHasTrades] - has trades - ", hasTrades)
+	return &pb.Response{Passed: hasTrades}, nil
+
 }
 
 func (s *server) InfoMarket(
 	ctx context.Context,
 	in *pb.InfoMarketRequest,
 ) (*pb.InfoMarketResponse, error) {
-	m := market.Look(in.Adress)
-	if m != nil {
-		buys := []uint64{}
-		for _, buy := range m.Pool.Buys {
-			buys = append(buys, buy.Offer)
-			buys = append(buys, buy.Recieve)
-			if len(buys) == 10 {
-				break
-			}
-		}
-		sells := []uint64{}
-		for _, sell := range m.Pool.Sells {
-			sells = append(sells, sell.Offer)
-			sells = append(sells, sell.Recieve)
-			if len(sells) == 10 {
-				break
-			}
-		}
-		fmt.Sprintln("[InfoMarket] - info abound market: ", m.Name)
-		return &pb.InfoMarketResponse{
-			MesKey:      m.MesKey,
-			Name:        m.Name,
-			Img:         m.Img,
-			Descr:       m.Descr,
-			OpCount:     m.OpCount,
-			Buys:        buys,
-			Sells:       sells,
-			ActiveBuys:  uint64(len(buys) / 2),
-			ActiveSells: uint64(len(sells) / 2),
-			InputFee:    m.InputFee,
-			OutputFee:   m.OutputFee,
-			WorkTime:    m.WorkTime,
-		}, nil
+	mkt := market.Look(in.Adress)
+	if mkt == nil {
+		fmt.Sprintln("[InfoMarket] - market not found")
+		return &pb.InfoMarketResponse{}, errors.New("market not found")
+
 	}
-	fmt.Sprintln("[InfoMarket] - market not found")
-	return &pb.InfoMarketResponse{}, errors.New("market not found")
+	buys := []uint64{}
+	for _, buy := range mkt.Pool.Buys {
+		buys = append(buys, buy.Offer)
+		buys = append(buys, buy.Recieve)
+		if len(buys) == 10 {
+			break
+		}
+	}
+	sells := []uint64{}
+	for _, sell := range mkt.Pool.Sells {
+		sells = append(sells, sell.Offer)
+		sells = append(sells, sell.Recieve)
+		if len(sells) == 10 {
+			break
+		}
+	}
+	fmt.Sprintln("[InfoMarket] - info abound market: ", mkt.Name)
+	return &pb.InfoMarketResponse{
+		MesKey:      mkt.MesKey,
+		Name:        mkt.Name,
+		Img:         mkt.Img,
+		Descr:       mkt.Descr,
+		OpCount:     mkt.OpCount,
+		Buys:        buys,
+		Sells:       sells,
+		ActiveBuys:  uint64(len(buys) / 2),
+		ActiveSells: uint64(len(sells) / 2),
+		InputFee:    mkt.InputFee,
+		OutputFee:   mkt.OutputFee,
+		WorkTime:    mkt.WorkTime,
+		Delimiter:   mkt.Delimiter,
+	}, nil
 }
 
 func (s *server) InfoSearch(
@@ -77,9 +80,6 @@ func (s *server) InfoSearch(
 	in *pb.InfoSearchRequest,
 ) (*pb.InfoSearchResponse, error) {
 	results := search.Search(in.Info)
-	if len(results) > 30 {
-		results = results[0:30]
-	}
 	fmt.Sprintln("[InfoSearch] - search results len: ", len(results))
 	return &pb.InfoSearchResponse{ConcMarkets: results}, nil
 }
@@ -89,24 +89,24 @@ func (s *server) InfoUser(
 	in *pb.InfoUserRequest,
 ) (*pb.InfoUserResponse, error) {
 	user := user.Look(in.Adress)
-	if user != nil {
-		adressesSlice := [][]byte{}
-		balancesSlice := []uint64{}
-		for strAdr, bal := range user.Balances {
-			adressesSlice = append(adressesSlice, []byte(strAdr))
-			balancesSlice = append(balancesSlice, bal)
-		}
-		fmt.Sprintln("[InfoUser] - info about user: ", user.PublicName)
-		return &pb.InfoUserResponse{
-			PublicName:     user.PublicName,
-			Balance:        user.Balance,
-			MesKey:         user.MesKey,
-			MarketAdresses: adressesSlice,
-			MarketBalances: balancesSlice,
-		}, nil
+	if user == nil {
+		fmt.Sprintln("[InfoUser] - error user not found")
+		return &pb.InfoUserResponse{}, errors.New("user not found")
 	}
-	fmt.Sprintln("[InfoUser] - error user not found")
-	return &pb.InfoUserResponse{}, errors.New("user not found")
+	adressesSlice := [][]byte{}
+	balancesSlice := []uint64{}
+	for strAdr, bal := range user.Balances {
+		adressesSlice = append(adressesSlice, []byte(strAdr))
+		balancesSlice = append(balancesSlice, bal)
+	}
+	fmt.Sprintln("[InfoUser] - info about user: ", user.PublicName)
+	return &pb.InfoUserResponse{
+		PublicName:     user.PublicName,
+		Balance:        user.Balance,
+		MesKey:         user.MesKey,
+		MarketAdresses: adressesSlice,
+		MarketBalances: balancesSlice,
+	}, nil
 }
 
 func (s *server) InfoMessages(
