7176ebdb21092d3a39db11ead482b4afaf4a3af6
DANCHE <dangdancheg@gmail.com>
2021-09-27 00:05:46 +0300

    .

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type server struct {
 func main() {
 	api_test_preparation.CreateNewUsers()
 	api_test_preparation.CreateNewMarkets()
+	api_test_preparation.FullFillWithTrades()
 	fmt.Println("Server has started! port: 8080 open")
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
