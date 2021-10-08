e59ae2a518f4499de6426536c835fb1fdd47a153
Danila Fomin <dangdancheg@gmail.com>
2021-10-08 15:37:07 +0300

    .

--- lock/lock.go
+++ lock/lock.go
@@ -1,1 +1,22 @@
 package lock
+
+import (
+	"time"
+)
+
+func Lock(ID []byte) {
+	var lockID [64]byte
+	copy(lockID[:], ID[:64])
+	keyByte := ID[0]
+	blocker := blockers[keyByte]
+	blocker.mutex.Lock()
+	_, found := blocker.userId[lockID]
+	if found {
+		blocker.mutex.Unlock()
+		time.Sleep(time.Millisecond * 144)
+		Lock(ID)
+		return
+	}
+	blocker.userId[lockID] = true
+	blocker.mutex.Unlock()
+}
