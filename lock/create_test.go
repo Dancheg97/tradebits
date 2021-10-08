67f33485dbb910b044be41573c9b4fd5cb2f56c6
Danila Fomin <dangdancheg@gmail.com>
2021-10-08 15:32:07 +0300

    .

--- lock/create_test.go
+++ lock/create_test.go
@@ -1,7 +1,6 @@
 package lock
 
 import (
-	"sync"
 	"testing"
 	"time"
 )
@@ -46,28 +45,5 @@ func TestTryToLockLocked(t *testing.T) {
 	case <-timeout:
 		t.Fatal("Test didn't finish in time")
 	case <-done:
-	}
-}
-
-type loka struct {
-	mu    sync.Mutex
-	count int
-}
-
-func LockAndUnlockWithTimer(lk *loka, t *testing.T) {
-	lk.mu.Lock()
-	lk.count += 1
-	time.Sleep(time.Second)
-	lk.mu.Unlock()
-}
-
-func TestLockMultipleRequests(t *testing.T) {
-	lk := loka{}
-	go LockAndUnlockWithTimer(&lk, t)
-	go LockAndUnlockWithTimer(&lk, t)
-	go LockAndUnlockWithTimer(&lk, t)
-	time.Sleep(time.Second * 5)
-	if lk.count != 3 {
-		t.Error("some locking stuff")
 	}
 }
