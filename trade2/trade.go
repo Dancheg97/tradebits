dce211dadfb66946bc1cea22bb4c4ee33be365d5
Danila Fomin <dangdancheg@gmail.com>
2021-10-11 15:25:05 +0300

    .

--- trade2/trade.go
+++ trade2/trade.go
@@ -1,1 +1,35 @@
 package trade2
+
+type trade struct {
+	Offer   uint64
+	Recieve uint64
+	Adress  []byte
+}
+
+func CreateTrade(
+	adress []byte,
+	revieve uint64,
+	offer uint64,
+) *trade {
+	if offer == 0 || revieve == 0 || adress == nil {
+		return nil
+	}
+	return &trade{
+		Offer:   offer,
+		Recieve: revieve,
+		Adress:  adress,
+	}
+}
+
+func (first trade) close(second *trade) bool {
+	if first.Offer >= second.Recieve && first.Recieve >= second.Offer {
+		firstRatio := float64(first.Offer) / float64(first.Recieve)
+		first.Offer = first.Offer - second.Recieve
+		first.Recieve = first.Recieve - second.Offer
+		secondRatio := float64(first.Offer) / float64(first.Recieve)
+		if secondRatio > firstRatio {
+			return true
+		}
+	}
+	return false
+}
--- trade2/trade_test.go
+++ trade2/trade_test.go
@@ -1,1 +1,20 @@
 package trade2
+
+import (
+	"sync_tree/calc"
+	"testing"
+)
+
+func TestCreateTrade(t *testing.T) {
+	trd := CreateTrade(calc.Rand(), 100, 120)
+	if trd == nil {
+		t.Error("This trade should be created normally")
+	}
+}
+
+func TestCreateTradeError(t *testing.T) {
+	trd := CreateTrade(calc.Rand(), 0, 12)
+	if trd != nil {
+		t.Error("this trade cannot be created")
+	}
+}
