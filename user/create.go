7fcac132425e7d01e9ee3a48a14f17295080b5cc
Danila Fomin <dangdancheg@gmail.com>
2021-11-17 21:25:02 +0300

    rfc user

---
+++ user/create.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"sync_tree/data"
+
+	filter "github.com/AccelByte/profanity-filter-go"
+)
+
+/*
+Create new user, in case there is already user with same adress
+the error will be logged
+*/
+func Create(adress []byte, mesKey []byte, name string) error {
+	if len(adress) != 64 {
+		return errors.New("bad adress length")
+	}
+	if len(name) > 12 {
+		return errors.New("name too big")
+	}
+	if len(mesKey) < 240 || len(mesKey) > 320 {
+		return errors.New("invalid message key length")
+	}
+	if data.Check(adress) {
+		return errors.New("possibly user already exists")
+	}
+	if data.Check([]byte(name)) {
+		return errors.New("user with that name exists")
+	}
+	if name[0] == " "[0] || name[len(name)-1] == " "[0] {
+		return errors.New("market name start/ends with space")
+	}
+	isBadName, _, _ := filter.Filter.ProfanityCheck(name)
+	if isBadName {
+		return errors.New("name contains bad words")
+	}
+	data.Put([]byte(name), []byte{})
+	u := user{
+		Balance:  0,
+		MesKey:   mesKey,
+		Name:     name,
+		Balances: make(map[string]uint64),
+		Messages: make(map[string][][]byte),
+		Arch:     map[string]string{},
+	}
+	cache := new(bytes.Buffer)
+	gob.NewEncoder(cache).Encode(u)
+	data.Put(adress, cache.Bytes())
+	return nil
+}
