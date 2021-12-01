cd175498068fc3bf59a70bd1d1d09f7b7b056109
Danila Fomin <dangdancheg@gmail.com>
2021-12-01 17:28:22 +0300

    create queue void

--- queue/create.go
+++ queue/create.go
@@ -1,1 +1,21 @@
-package 
+package queue
+
+import "sync"
+
+type queue struct {
+	mu     sync.Mutex
+	values []KvPair
+}
+
+type KvPair struct {
+	Key   []byte
+	Value []byte
+}
+
+func CreateQueue() *queue {
+	queue := queue{
+		mu:     sync.Mutex{},
+		values: make([]KvPair, 1000),
+	}
+	return &queue
+}
