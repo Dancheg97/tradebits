bfe1791db96d64c6114131e03275f5e8a6ab9cba
Danila Fomin <dangdancheg@gmail.com>
2021-12-06 12:04:15 +0300

    added file for config t0 make a preset wether node is starting new network or connection to an existing one

--- test/x.go
+++ test/x.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"path"
-	"runtime"
-
-	"github.com/syndtr/goleveldb/leveldb"
+	"fmt"
+	"io/ioutil"
 )
 
-func openDB() *leveldb.DB {
-	_, filename, _, _ := runtime.Caller(0)
-	dbPath := path.Dir(filename) + "/leveldb"
-	db, openErr := leveldb.OpenFile(dbPath, nil)
-	if openErr != nil {
-		return nil
-	}
-	return db
+func main() {
+	filebytes, _ := ioutil.ReadFile("adr.pem")
+	fmt.Println(string(filebytes))
 }
-
