9ae30d27de1f85c40dfff332656d5ca04c65046b
Danila Fomin <dangdancheg@gmail.com>
2021-12-06 11:04:39 +0300

    added connection server to server pool

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net"
 
 	pb "sync_tree/api"
-	"sync_tree/prepare"
 
 	"google.golang.org/grpc"
 )
@@ -15,12 +14,10 @@ type server struct {
 	pb.UnimplementedInfoServer
 	pb.UnimplementedMarketServer
 	pb.UnimplementedUserServer
+	pb.UnimplementedConnectionServer
 }
 
 func main() {
-	prepare.CreateNewUsers()
-	prepare.CreateNewMarkets()
-	prepare.FullFillWithTrades()
 	fmt.Println("Server has started! port: 8080 open")
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
