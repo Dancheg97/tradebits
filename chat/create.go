0b4aef796a4acc7d6f957ca1a8a5746336da9cde
Danila Fomin <dangdancheg@gmail.com>
2021-12-22 12:59:09 +0300

    added logic to create chat between user and market

--- chat/create.go
+++ chat/create.go
@@ -1,1 +1,35 @@
 package chat
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"orb/data"
+)
+
+type chat struct {
+	Messages []string
+}
+
+func createChat(marketAdress []byte, userAdress []byte) error {
+	mktExists := data.Check(marketAdress)
+	if !mktExists {
+		return errors.New("market does not exist")
+	}
+	ustExists := data.Check(userAdress)
+	if !ustExists {
+		return errors.New("user does not exis")
+	}
+	chatAdress := append(marketAdress, userAdress...)
+	chtExists := data.Check(chatAdress)
+	if chtExists {
+		return errors.New("chat already exists")
+	}
+	cht := chat{
+		Messages: []string{},
+	}
+	cache := new(bytes.Buffer)
+	gob.NewEncoder(cache).Encode(cht)
+	data.Put(chatAdress, cache.Bytes())
+	return nil
+}
--- go.mod
+++ go.mod
@@ -8,5 +8,4 @@ require (
 	github.com/syndtr/goleveldb v1.0.0
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
-	gopkg.in/yaml.v2 v2.4.0
 )
