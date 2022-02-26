78593e08f7b98ae06e3e84feb04bb101c70dd2ce
Danila Fomin <dangdancheg@gmail.com>
2022-02-26 22:37:23 +0300

    started working on graphql schema of application data

--- chat/create.go
+++ chat/create.go
@@ -1,42 +1,9 @@
 package chat
 
-import (
-	"bytes"
-	"encoding/gob"
-	"errors"
-	"orb/data"
-)
-
-type Message struct {
-	message  string
-	fromUser bool
+type Chat struct {
+	Messages []string `json:"messages"`
 }
 
-type chat struct {
-	Messages        []Message
-	checkedByUser   bool
-	checkedByMarket bool
-}
-
-func createChat(marketAdress []byte, userAdress []byte) error {
-	mktExists := data.Check(marketAdress)
-	if !mktExists {
-		return errors.New("market does not exist")
-	}
-	ustExists := data.Check(userAdress)
-	if !ustExists {
-		return errors.New("user does not exis")
-	}
-	chatAdress := append(marketAdress, userAdress...)
-	chtExists := data.Check(chatAdress)
-	if chtExists {
-		return errors.New("chat already exists")
-	}
-	cht := chat{
-		Messages: []Message{},
-	}
-	cache := new(bytes.Buffer)
-	gob.NewEncoder(cache).Encode(cht)
-	data.Put(chatAdress, cache.Bytes())
-	return nil
+func getMessages() {
+	
 }
---
+++ dgraph/open_test.go
@@ -0,0 +1,22 @@
+package dgraph
+
+import (
+	"testing"
+)
+
+func TestOpenDgraphSuccess(t *testing.T) {
+	dgraphClient := newClient("localhost:9080")
+	if dgraphClient == nil {
+		t.Error("error opening dgraph instance")
+	}
+	dgraphClient.NewTxn()
+}
+
+func TestOpenDgraphError(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	newClient("")
+}
