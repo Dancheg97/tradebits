53dd9526c2fef8a0086f7d19cc4076336515efea
DANCHE <dangdancheg@gmail.com>
2022-03-01 22:46:53 +0300

    pushed repo to dockerhub so that evryone can access that

--- main.go
+++ main.go
@@ -13,20 +13,13 @@ import (
 	"log"
 	"net/http"
 
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//    sw "github.com/myname/myrepo/go"
-	//
-	sw "./go"
+	"tradebits/swagger"
 )
 
 func main() {
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+	router := swagger.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
