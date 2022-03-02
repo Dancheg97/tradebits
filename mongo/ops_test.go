d57d6d9a114812cdd76309a7b462a1668657446c
DANCHE <dangdancheg@gmail.com>
2022-03-02 19:34:20 +0300

    added update test for mongodb module

--- mongo/ops_test.go
+++ mongo/ops_test.go
@@ -1,9 +1,78 @@
 package mongo
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func setupTestEnv(collectioname string) {
+	openMongoFromEnv()
+	CreateCollection(collectioname)
+	CreateIndex(collectioname, "key", "hashed")
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		database.Collection(collectioname).Drop(context.Background())
+	}()
+}
+
+func TestPut(t *testing.T) {
+	collectionname := "testputcol"
+	setupTestEnv(collectionname)
+	err := Put(collectionname, &map[string]string{
+		"key": "testputkey",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	time.Sleep(time.Millisecond * 200)
+}
 
 func TestCheck(t *testing.T) {
-	OpenMongo("mongodb://localhost:27017")
-	CreateCollection("test")
-	CreateIndex("key", "")
+	collectionname := "testcheckcol"
+	setupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key": "testcheckkey",
+	})
+	found := Check("testcheckkey", collectionname)
+	if !found {
+		t.Error("not found")
+	}
+	time.Sleep(time.Millisecond * 200)
+}
+
+func TestGet(t *testing.T) {
+	collectionname := "testgetcol"
+	setupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key":   "testgetkey",
+		"vaval": "tester",
+	})
+	mp := map[string]string{}
+	Get("testgetkey", collectionname, &mp)
+	if mp["vaval"] != "tester" {
+		t.Error("mongo returned bad value")
+	}
+	time.Sleep(time.Millisecond * 200)
+}
+
+func TestUpdate(t *testing.T) {
+	collectionname := "testupdatecol"
+	setupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key":   "testupdatekey",
+		"vaval": "tester",
+	})
+	rez := Update("testupdatekey", collectionname, &map[string]string{
+		"key":   "testupdatekey",
+		"vaval": "tester2",
+	})
+	if rez != nil {
+		t.Error("Error with updating")
+	}
+	mp := map[string]string{}
+	Get("testupdatekey", collectionname, &mp)
+	if mp["vaval"] != "tester2" {
+		t.Error("mongo did not update value in database")
+	}
 }
