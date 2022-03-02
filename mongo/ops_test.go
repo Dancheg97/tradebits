5ab586aec6db2a188925c4c5da78421780ef15e3
Danila Fomin <dangdancheg@gmail.com>
2022-03-02 13:29:38 +0300

    added tests for mongodb operations

--- mongo/ops_test.go
+++ mongo/ops_test.go
@@ -1,43 +1,67 @@
 package mongo
 
-// import (
-// 	"context"
-// 	"testing"
-// 	"time"
-// )
+import (
+	"context"
+	"testing"
+	"time"
+)
 
-// func TestUserCheck(t *testing.T) {
-// 	time.Sleep(time.Millisecond * 200)
-// 	Setup("mongodb://localhost:27017")
-// 	user.
-// 		UserCreate("12")
-// 	exists := UserCheck("12")
-// 	if !exists {
-// 		t.Error("user does not exist")
-// 	}
-// 	userCollection.Drop(context.Background())
-// }
+func SetupTestEnv(collectioname string) {
+	OpenMongo("mongodb://localhost:27017")
+	CreateCollection(collectioname)
+	CreateIndex(collectioname, "key", "hashed")
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		database.Collection(collectioname).Drop(context.Background())
+	}()
+}
 
-// func TestUserCreate(t *testing.T) {
-// 	time.Sleep(time.Millisecond * 400)
-// 	Setup("mongodb://localhost:27017")
-// 	err := UserCreate("123")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	userCollection.Drop(context.Background())
-// }
+func TestPut(t *testing.T) {
+	collectionname := "testputcol"
+	SetupTestEnv(collectionname)
+	err := Put(collectionname, &map[string]string{
+		"key": "testputkey",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	time.Sleep(time.Millisecond * 200)
+}
 
-// func TestUserGet(t *testing.T) {
-// 	time.Sleep(time.Millisecond * 600)
-// 	Setup("mongodb://localhost:27017")
-// 	UserCreate("123")
-// 	user, err := UserGet("123")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	if user.Pubkey != "123" {
-// 		t.Error("wrong field")
-// 	}
-// 	userCollection.Drop(context.Background())
-// }
+func TestCheck(t *testing.T) {
+	collectionname := "testcheckcol"
+	SetupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key": "testcheckkey",
+	})
+	found := Check("testcheckkey", collectionname)
+	if !found {
+		t.Error("not found")
+	}
+	time.Sleep(time.Millisecond * 200)
+}
+
+func TestGet(t *testing.T) {
+	collectionname := "testgetcol"
+	SetupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key":   "testgetkey",
+		"vaval": "tester",
+	})
+	mp := map[string]string{}
+	Get("testgetkey", collectionname, &mp)
+	if mp["vaval"] != "tester" {
+		t.Error("retrieved bad value")
+	}
+	time.Sleep(time.Millisecond * 200)
+}
+
+func TestUpdate(t *testing.T) {
+	collectionname := "testupdatecol"
+	SetupTestEnv(collectionname)
+	Put(collectionname, &map[string]string{
+		"key":   "testupdatekey",
+		"vaval": "tester",
+	})
+	
+}
