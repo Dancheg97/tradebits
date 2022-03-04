85a85df9ffe6d27ce6460749bf03b9e4ef10f58c
Danila Fomin <dangdancheg@gmail.com>
2022-03-04 21:12:20 +0300

    chaging swagger structure

--- crypt/setup_test.go
+++ crypt/setup_test.go
@@ -1,23 +1,22 @@
 package crypt
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"testing"
 )
 
-func getTestKey() string {
-	return `-----BEGIN RSA PRIVATE KEY-----
-MIIBOgIBAAJBAL+lgvRKvBFhN8ZsfXJ9L8GOsnh/k1mhRZU0GtMBN0ODZfrZtawo
-9oU/H2NHEhFTZUCIeMAfDMUnW+Mozy7cM2sCAwEAAQJAbhGZTMU/jpvPdN8KjZ7o
-trpGNn7PGkNNu4vZfCbOnq7yc44lg9LH6ME+XJ5mil1BfULyf4wN+z6BUSkgc5Ed
-AQIhAOPEk3p5N/4qNfLgxo7rAMyaWXf7G8XzGpnsRbcFYN4RAiEA12a/8IjoSyTV
-3v1h252QYUv+5/EKXeUr9UaG6ZwBLbsCIBIHybT0S55vMP4dFRrTN6j6vwJkenpd
-GEn/DjXC0zxBAiEAv6DpoVQrVK6wlnFVXcwPZn/1huMqFd6L/OmBnNLtOYECIDV9
-sr1ru20w//oaYzsK98D7AdYBMadvOn9sIiSa6Zrh
------END RSA PRIVATE KEY-----`
-}
-
 func TestSetup(t *testing.T) {
-	rez := Setup(getTestKey())
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	keyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	)
+	rez := Setup(string(keyPEM))
 	if rez != nil {
 		t.Error(rez)
 	}
--- crypt/sign_test.go
+++ crypt/sign_test.go
@@ -1,23 +1,21 @@
 package crypt
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"testing"
 )
 
-func getTestSign() string {
-	return `nHmOEHJC+jXdtGdNeEhPhzjtDtGBo/SJ3SSEa/PT45+Dpasmz3K4Hp9HHF2l0RSriALn8EtegV83wBvn0LKRjg==`
-}
-
 func TestSign(t *testing.T) {
-	key := getTestKey()
-	realsign := getTestSign()
-	mes := "hello"
-	Setup(key)
-	outsign, err := Sign(mes)
-	if err != nil {
-		t.Error(err)
-	}
-	if outsign != realsign {
-		t.Error(`real sign is not matching with actual`)
-	}
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	keyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	)
+	Setup(string(keyPEM))
+	Sign("")
 }
