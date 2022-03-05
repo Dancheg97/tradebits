3b793ded3e8bbb95729d5b17fb522ac5c1a2d593
DANCHE <dangdancheg@gmail.com>
2022-03-05 12:17:02 +0300

    added private and public key to configuration

--- crypt/setup_test.go
+++ crypt/setup_test.go
@@ -9,13 +9,14 @@ import (
 )
 
 func TestSetup(t *testing.T) {
-	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	key, _ := rsa.GenerateKey(rand.Reader, 8192)
 	keyPEM := pem.EncodeToMemory(
 		&pem.Block{
 			Type:  "RSA PRIVATE KEY",
 			Bytes: x509.MarshalPKCS1PrivateKey(key),
 		},
 	)
+	t.Error(string(keyPEM))
 	rez := Setup(string(keyPEM))
 	if rez != nil {
 		t.Error(rez)
--- crypt/sign.go
+++ crypt/sign.go
@@ -11,10 +11,15 @@ import (
 func Sign(message string) (string, error) {
 	h := sha512.New()
 	h.Write([]byte(message))
-	d := h.Sum(nil)
-	sign, signError := rsa.SignPSS(rand.Reader, priv, crypto.SHA512, d, nil)
-	if signError != nil {
-		return "", signError
+	sign, err := rsa.SignPSS(
+		rand.Reader,
+		priv,
+		crypto.SHA512,
+		h.Sum(nil),
+		nil,
+	)
+	if err != nil {
+		return "", err
 	}
 	signBase64 := base64.StdEncoding.EncodeToString(sign)
 	return signBase64, nil
