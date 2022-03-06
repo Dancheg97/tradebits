44931ee4f3faaca7f142dd7cee45325087c08cd6
DANCHE <dangdancheg@gmail.com>
2022-03-06 17:14:24 +0300

    refactoring all modules to increase testability of API package

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"tradebits/api"
+	"tradebits/crypter"
+	"tradebits/mongoer"
+	"tradebits/rediser"
 
 	"github.com/joho/godotenv"
 )
@@ -20,10 +24,29 @@ func readConfigField(field string) string {
 
 func init() {
 	godotenv.Load()
-	initCrypt()
-	initResis()
-	initMongo()
-	initSwagger()
+	readConfigField("REDIS_HOST")
+	crypt, err1 := crypter.Get(readConfigField("MARKET_PRIVATEKEY"))
+	redis, err2 := rediser.Get(readConfigField("REDIS_HOST"))
+	mongo, err3 := mongoer.Get(
+		readConfigField("MONGO_HOST"),
+		readConfigField("MONGO_NAME"),
+		readConfigField("MONGO_PASSWORD"),
+		readConfigField("MONGO_DB"),
+	)
+	m := map[string]string{
+		"name":      readConfigField("MARKET_NAME"),
+		"mkey":      crypt.Pub(),
+		"descr":     readConfigField("MARKET_DESCR"),
+		"img":       readConfigField("MARKET_IMG"),
+		"worktime":  readConfigField("MARKET_WORKTIME"),
+		"fee":       readConfigField("MARKET_FEE"),
+		"delimiter": readConfigField("MARKET_DELIMITER"),
+	}
+	respbytes, err4 := json.Marshal(m)
+	if !(err1 != nil && err2 != nil && err3 != nil && err4 != nil) {
+		log.Fatal("Setup api error: ", err1, err2, err3, err4)
+	}
+	api.Setup(respbytes, mongo, crypt, redis)
 }
 
 func main() {
