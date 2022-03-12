01933ec14e2f6f0e27bb48b7d434c3c0268fea6e
DANCHE <dangdancheg@gmail.com>
2022-03-12 23:57:52 +0300

    added requst to verify market connection for order approval

--- connect/connect.go
+++ connect/connect.go
@@ -2,21 +2,26 @@ package connect
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
 	"tradebits/crypter"
 )
 
 type ConnectInfo struct {
 	ConnectAdress string
+	ConnectKey    string
 	SelfName      string
 	SelfAdress    string
 }
 
 type ConnectRequest struct {
-	Hhey       string `json:"hhey"`
-	Mkey       string `json:"mkey"`
-	Marketname string `json:"marketname"`
-	Marketlink string `json:"marketlink"`
-	Sign       string `json:"sign"`
+	Hhey  string `json:"hhey"`
+	Mkey  string `json:"mkey"`
+	Mname string `json:"marketname"`
+	Madr  string `json:"marketlink"`
+	Sign  string `json:"sign"`
 }
 
 func Connect(crypt crypter.ICrypter, info ConnectInfo) error {
@@ -29,5 +34,30 @@ func Connect(crypt crypter.ICrypter, info ConnectInfo) error {
 	if err != nil {
 		return err
 	}
-
+	request, err := json.Marshal(ConnectRequest{
+		Hhey:  info.ConnectKey,
+		Mkey:  crypt.Pub(),
+		Mname: info.SelfName,
+		Madr:  info.SelfAdress,
+		Sign:  sign,
+	})
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewBuffer(request)
+	req, err := http.NewRequest("PUT", info.ConnectAdress, buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	code := resp.StatusCode
+	if code != 200 {
+		return errors.New("Bad status code on response " + strconv.Itoa(code))
+	}
+	return nil
 }
